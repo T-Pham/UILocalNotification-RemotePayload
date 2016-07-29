cf2f1dc63350c07d0033fd4295a7419b2e39ae4a
Thanh Pham <minhthanh@me.com>
2016-07-29 15:22:25 +0800

    Initial Commit

---
+++ UILocalNotification-RemotePayload/UILocalNotification-RemotePayload.h
@@ -0,0 +1,19 @@
+//
+//  UILocalNotification-RemotePayload.h
+//  UILocalNotification-RemotePayload
+//
+//  Created by Thanh Pham on 7/29/16.
+//  Copyright © 2016 TPM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UILocalNotification-RemotePayload.
+FOUNDATION_EXPORT double UILocalNotification_RemotePayloadVersionNumber;
+
+//! Project version string for UILocalNotification-RemotePayload.
+FOUNDATION_EXPORT const unsigned char UILocalNotification_RemotePayloadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UILocalNotification_RemotePayload/PublicHeader.h>
+
+
---
+++ UILocalNotification-RemotePayloadTests/UILocalNotification_RemotePayloadTests.swift
@@ -0,0 +1,36 @@
+//
+//  UILocalNotification_RemotePayloadTests.swift
+//  UILocalNotification-RemotePayloadTests
+//
+//  Created by Thanh Pham on 7/29/16.
+//  Copyright © 2016 TPM. All rights reserved.
+//
+
+import XCTest
+@testable import UILocalNotification_RemotePayload
+
+class UILocalNotification_RemotePayloadTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
